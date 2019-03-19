ba49d29ccdf04ae61942d8ebb09a958455eb71be
onkanya <onkanyalearning@gmail.com>
2019-03-19 21:36:57 +0700

    git merge

--- routes/requestlicense.js
+++ routes/requestlicense.js
@@ -3,7 +3,7 @@ var db = require('../config')
 module.exports = function(app) {
     app.post('/newrequest', function (req, res) {
         try {
-            query = `
+            db.query(`
                 INSERT INTO requestlicense
                 (
                     RLTid, Cid, RLnorequest,
@@ -22,8 +22,57 @@ module.exports = function(app) {
                     '${req.body.Did}', '${req.body.Pid}', '${req.body.RLtel}',
                     '${req.body.RLemail}', '${req.body.RLdetail}', '${req.body.Uid}'
                 )
-            `
-            db.query(query)
+            `)
+            res.send({
+                status: 'success'
+            })
+        } catch (error) {
+            console.log(error)
+            return
+        }
+    })
+    
+    app.post('/updaterequest/:id', function (req, res) {
+        try {
+            console.log(req.body)
+            db.query(`
+                UPDATE requestlicense
+                SET RLTid = '${req.body.RLTid}',
+                    Cid = '${req.body.Cid}',
+                    RLnorequest = '${req.body.RLnorequest}',
+                    RLdate = '${req.body.RLdate}',
+                    Prefixid = '${req.body.Prefixid}',
+                    RLfname = '${req.body.RLfname}',
+                    RLlname = '${req.body.RLlname}',
+                    RLage = '${req.body.RLage}',
+                    RLnationality = '${req.body.RLnationality}',
+                    RLhomeno = '${req.body.RLhomeno}',
+                    RLmoo = '${req.body.RLmoo}',
+                    RLsoi = '${req.body.RLsoi}',
+                    RLroad = '${req.body.RLroad}',
+                    RLvillage = '${req.body.RLvillage}',
+                    SDTid = '${req.body.SDTid}',
+                    Did = '${req.body.Did}',
+                    Pid = '${req.body.Pid}',
+                    RLtel = '${req.body.RLtel}',
+                    RLemail = '${req.body.RLemail}',
+                    RLdetail = '${req.body.RLdetail}'
+                WHERE RLid = ` + req.params.id)
+            res.send({
+                status: 'success'
+            })
+        } catch (error) {
+            console.log(error)
+            return
+        }
+    })
+    
+    app.post('/deleterequest/:id', function (req, res) {
+        try {
+            console.log(req.body)
+            db.query(`
+                DELETE FROM requestlicense
+                WHERE RLid = ` + req.params.id)
             res.send({
                 status: 'success'
             })
@@ -39,5 +88,18 @@ module.exports = function(app) {
             if(err) throw err
             res.send(result)
         })
+    })
+    
+    app.get('/getrequest/:id', function (req, res) {
+        try {
+            db.query(`SELECT * FROM requestlicense WHERE RLid = ` + req.params.id , (err, result, f) => {
+                if(err) throw err
+                res.send(result)
+            })
+        }
+        catch (error) {
+            console.log(error)
+            return
+        }
     })
 }
